019f155edd7403e7e158f6bcb467fdb1aefec243
roalwh <nmnmnmlk6248@gmail.com>
2023-07-31 00:08:56 +0900

    20230731

---
+++ spring3-Backend/src/main/java/pe/roalwh/backend/controller/BlogViewController.java
@@ -0,0 +1,28 @@
+﻿package pe.roalwh.backend.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import pe.roalwh.backend.dto.ArticleListViewResponse;
+import pe.roalwh.backend.service.BlogService;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+  private final BlogService blogService;
+
+  @GetMapping("/articles")
+  public String getArticles(Model model) {
+    List<ArticleListViewResponse> article = blogService.findAll().stream()
+        .map(ArticleListViewResponse::new)
+        .toList();
+    model.addAttribute("articles", article);
+
+    return "articleList";
+  }
+}
---
+++ spring3-Backend/src/main/java/pe/roalwh/backend/dto/ArticleListViewResponse.java
@@ -0,0 +1,23 @@
+﻿package pe.roalwh.backend.dto;
+
+
+import org.springframework.stereotype.Controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import pe.roalwh.backend.model.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+  private final Long id;
+  private final String title;
+  private final String content;
+
+  public  ArticleListViewResponse(Article article){
+
+    this.id = article.getId();
+    this.title = article.getTitle();
+    this.content = article.getContent();
+  }
+}
